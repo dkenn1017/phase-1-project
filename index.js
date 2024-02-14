805b70ad0514c915943aa2eb1609dd1410060332
darrylkennedy <darrylkennedy13@gmail.com>
2024-02-13 21:21:47 -0500

    added 2 event listeners

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => console.log("we are connected!!!"))
+document.addEventListener("DOMContentLoaded", () => console.log("We are connected!"))
+
+const all_img = document.querySelectorAll("img")
+all_img.forEach(function(img){
+  img.addEventListener("mouseover", function(){
+    console.log("hovered over")
+  })
+})
+
+const all_btn = document.querySelectorAll("button");
+all_btn.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+        console.log("button clicked")
+   });
+});
+
